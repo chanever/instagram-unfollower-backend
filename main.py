85cc2416db96ab70440cd3807c472c492726fe13
Chan <chan@Chanui-MacBookPro.local>
2025-08-01 12:09:27 +0900

    www added rn

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "http://localhost:5173", 
         "http://localhost:3000", 
         "https://instagram-unfollower-frontend.vercel.app",
         "https://insta-dive.com",
